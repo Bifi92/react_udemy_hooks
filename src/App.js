1ec73b124afd6f3013faa64d74c080791cd08e9a
Bifi92 <bifi@pop-os.localdomain>
2022-07-17 12:18:38 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
+
+  const [tarefas, setTarefas] = useState([]);
+  const [input, setInput] = useState('');
+
+  useEffect(
+    ()=>{
+      const tarefasStorage = localStorage.getItem('tarefas');
+      if(tarefasStorage) { //se tem valor na variavel
+        setTarefas(JSON.parse(tarefasStorage));
+      }
+    }
+    ,[]
+  ); //com o segundo parametro vazio, o trigger 'e a app.
+     //executa quando monta a tela (componentDidMount)
+
+  useEffect(
+    ()=>{
+      localStorage.setItem('tarefas', JSON.stringify(tarefas));
+      return ()=>{}//executa quando desmontar o componente (componentWillUnmount)
+    }
+    , [tarefas]
+  ); //executa quando o(os) componente passado no segundo parametro sofre alteracao (ComponentDidUpdate)
+
+  const handleAdd = useCallback(
+    ()=>{
+      setTarefas([...tarefas, input])
+      setInput('');
+    }
+    , [tarefas, input]
+  ) //executa a funcao passada no primeiro parametro. O segundo parametro sao os hooks utilizados na funcao do primeiro.
+
+  // function handleAdd(){
+  //   setTarefas([...tarefas, input])
+  //   setInput('');
+  // }
+
+  const totalTarefas = useMemo(()=>tarefas.length, [tarefas]); //Executa o primeiro parametro sempre que sempre que o segundo parametro sofre alteracao.
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tarefas.map(
+            tarefa => (
+              <li key={tarefa}>
+                {tarefa}
+              </li>
+            )
+          )
+        }
+      </ul>
+      <br/>
+      <strong>Voce tem {totalTarefas} tarefa(as)</strong>
+      <br/>
+      <input type='text' value={input} onChange={e=>setInput(e.target.value)}/>
+      <button type='button' onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }
